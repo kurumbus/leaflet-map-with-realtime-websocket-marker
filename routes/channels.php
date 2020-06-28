7769113455763508470d292d3277e1f491cc56ab
kurumbus <grigory.shein@gmail.com>
2020-06-29 06:16:41 +0700

    broadcasting base

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('private-my-channel', function () {
+    return true;
+});
+
+Broadcast::channel('my-channel', function ($user) {
+    return true;
+});
